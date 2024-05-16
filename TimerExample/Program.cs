4c744a68d297e9360db9139e743fb538de72be3d
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-15 21:34:44 -0500

    Add files via upload

---
+++ TimerExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Timers;
+
+namespace TimerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elasped;
+        }
+
+        private static void MyTimer_Elasped(object sender, ElapsedEventArgs)
+        {
+            Console.WriteLine("Elasped: ");
+        }
+    }
+}
